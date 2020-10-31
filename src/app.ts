81715b4be6ec3f287312c05833d38d1abf2e3792
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-10-31 17:02:37 +1100

    Function And Callback types

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,16 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandler(n1: number, n2: number, cb: (num: number) => void) {
+  // checks if cb is a function with a number paramater and what it will ignore what it returns as of the ignore
+  cb(n1 + n2);
+}
+
 let combineValues: (a: number, b: number) => number; // only accepts functions that have 2 params that are numbers, and returns a number
 combineValues = add; // Add has 2 params that are numbers, and returns a number
 
 console.log(combineValues(8, 8)); // 16
+
+addAndHandler(10, 20, (a) => {
+  console.log(a);
+});
