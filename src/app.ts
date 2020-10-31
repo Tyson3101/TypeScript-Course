adf4f8c28344f549bb5f0fc19f0009485552ec2c
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-10-31 17:13:40 +1100

    never type

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,12 @@
-let userInputUnknown: unknown;
-let userNameUnknown: string;
-
-let userInputAny: any;
-let userNameAny: string;
-
-userInputUnknown = 3;
-userInputUnknown = "Tyson";
-userInputUnknown = userInputUnknown; // Error: Type 'unknown' is not assignable to type 'string'
-
-userInputAny = 3;
-userInputAny = "Tyson";
-userNameAny = userInputAny; // No Error
-
-/*
-  Do extra type check with unknown as it can check if is string, while any wont check and just assign it self
-*/
-if (typeof userInputUnknown === "string") {
-  userNameUnknown = userInputUnknown;
+function gernerateError(message: string, code: number): never {
+  // gives return value as never
+  throw {
+    message: message,
+    errorCode: code,
+  };
 }
 
-console.log(userNameUnknown);
+let result = gernerateError("an error errcured", 400);
+console.log(result); // logs nothing, while void does, basically crashes script (need try catch to keep going)
+
+// while(true) {} also returns never
