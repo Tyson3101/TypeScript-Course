823b6f58026de2e7442005ff193869a29b2d9301
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-10-31 15:18:55 +1100

    Type checking in function paramaters

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+function add(n1: number, n2: number, showResult: boolean, prefix: string) {
+  if (showResult) console.log(prefix, n1 + n2);
+  else return n1 + n2;
+}
+
+const number1 = 6;
+const number2 = 2.8;
+const printResult = true;
+const resultPrefix = "The Number returned:";
+
+add(number1, number2, printResult, resultPrefix);
