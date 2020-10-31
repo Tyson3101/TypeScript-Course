89d5bab31996956d1aa77b9656d36b64893c7390
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-10-31 16:07:19 +1100

    Enums

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
-const person: {
-  name: string;
-  favNumber: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+enum Role {
+  ADMIN = 5, // ADMIN = Starting Number (Text or whatever), default 0
+  READ_ONLY, // defualt plus 1 of last element of enum, or can redefine with number, string,etc
+  AUTHOR, // same as read only
+}
+
+const person = {
   name: "Tyson",
   favNumber: 4,
   hobbies: ["Coding", "Gaming"], // Type string[]
-  role: [2, "author"], // type (number | string)[]
+  role: Role.ADMIN, // type (number | string)[]
 };
 
-person.role.push("admin"); // Only want 2 elements
-
-let favoriteActivities: any[]; // Anything can go in array
-favoriteActivities = ["Hey", 25];
-
-console.log(person.name);
+if (person.role === 0) {
+  console.log(" Is Admin!");
+}
