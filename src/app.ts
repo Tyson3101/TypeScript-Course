6d4f923dc1cc5c242e80669f16a5f96fb8e75d15
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-10-31 15:50:19 +1100

    Array type checking

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-const person: {
-  name: string;
-  favNumber: number;
-} = {
+const person = {
   name: "Tyson",
   favNumber: 4,
+  hobbies: ["Coding", "Gaming"], // Type string[]
 };
+
+let favoriteActivities: any[]; // Anything can go in array
+favoriteActivities = ["Hey", 25];
+
+for (let hobbie of person.hobbies) {
+  console.log(hobbie.toLowerCase()); // Already knows person.hobbies is type string[]
+}
 
 console.log(person.name);
