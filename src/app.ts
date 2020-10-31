81ce57e5315905b85ca83f43de3f9ef9c01785c8
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-10-31 22:17:37 +1100

    More on type checking variables, about any, and typescript assuming types

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,9 @@
-/*
-This returns a HTMLElement, which doesnt have have a .value property
-While, we need a HTMLInputElement, which does
-This is why we need (type assertions)
-const myInput = document.getElementById("myInp"); // Returns HTMLElement
-const myInput = document.getElementById("myInp") as HTMLInputElement; // Returns HTMLInputElement
-const myInput = <HTMLInputElement>document.getElementById("myInp"); // Returns HTMLInputElement
-We type asserted this to a HTMLInputElement
+let thisStayNumber = 5; // typescript guesses that we want to make this stay a number
+thisStayNumber = "Error 5"; // Errors
+thisStayNumber = [" Error 5"]; // Errors
+thisStayNumber = null; // Doesnt error
 
-Example with Discord.js
-client.channels.cache.get('12345678912') // returns a channel, which deosnt .send() method, only on textbasedchannels
-client.channels.cache.get('12345678912') as TextChannel // returns a TextChannel, which has .send() method
-<TextChannel>client.channels.cache.get('12345678912') // returns a TextChannel, which has .send() method
-*/
-
-const myInputHTMLElement = document.getElementById("myInp"); // Returns HTMLElement
-const myInput1 = <HTMLInputElement>document.getElementById("myInp"); // Returns HTMLInputElement
-const myInput2 = document.getElementById("myInp") as HTMLInputElement; // Returns HTMLInputElement
+let thisDoesntStayNumber: any = 5; // Like said before, any basically ignores type checking
+thisDoesntStayNumber = " no Error 5"; // Doesnt Erorr
+thisDoesntStayNumber = [" no Error 5"]; // Doesnt Error
+thisDoesntStayNumber = null; // Doesnt error
