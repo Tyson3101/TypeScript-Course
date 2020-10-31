7e814e77d543c54654057132f1005e1f9795b87b
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-10-31 17:07:51 +1100

    Unkown Type (A bit of any)

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,22 @@
-function add(
-  n1: number,
-  n2: number
-): number /* type checks the return value data type as number*/ {
-  return n1 + n2; // this returns data type number
+let userInputUnknown: unknown;
+let userNameUnknown: string;
+
+let userInputAny: any;
+let userNameAny: string;
+
+userInputUnknown = 3;
+userInputUnknown = "Tyson";
+userInputUnknown = userInputUnknown; // Error: Type 'unknown' is not assignable to type 'string'
+
+userInputAny = 3;
+userInputAny = "Tyson";
+userNameAny = userInputAny; // No Error
+
+/*
+  Do extra type check with unknown as it can check if is string, while any wont check and just assign it self
+*/
+if (typeof userInputUnknown === "string") {
+  userNameUnknown = userInputUnknown;
 }
 
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-function addAndHandler(n1: number, n2: number, cb: (num: number) => void) {
-  // checks if cb is a function with a number paramater and what it will ignore what it returns as of the ignore
-  cb(n1 + n2);
-}
-
-let combineValues: (a: number, b: number) => number; // only accepts functions that have 2 params that are numbers, and returns a number
-combineValues = add; // Add has 2 params that are numbers, and returns a number
-
-console.log(combineValues(8, 8)); // 16
-
-addAndHandler(10, 20, (a) => {
-  console.log(a);
-});
+console.log(userNameUnknown);
