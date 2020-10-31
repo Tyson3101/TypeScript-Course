69dc0746a7383867c6f53271f0321617cbf8f590
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-10-31 22:10:39 +1100

    Type Assertions

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-function gernerateError(message: string, code: number): never {
-  // gives return value as never
-  throw {
-    message: message,
-    errorCode: code,
-  };
-}
+/*
+This returns a HTMLElement, which doesnt have have a .value property
+While, we need a HTMLInputElement, which does
+This is why we need (type assertions)
+const myInput = document.getElementById("myInp"); // Returns HTMLElement
+const myInput = document.getElementById("myInp") as HTMLInputElement; // Returns HTMLInputElement
+const myInput = <HTMLInputElement>document.getElementById("myInp"); // Returns HTMLInputElement
+We type asserted this to a HTMLInputElement
 
-let result = gernerateError("an error errcured", 400);
-console.log(result); // logs nothing, while void does, basically crashes script (need try catch to keep going)
+Example with Discord.js
+client.channels.cache.get('12345678912') // returns a channel, which deosnt .send() method, only on textbasedchannels
+client.channels.cache.get('12345678912') as TextChannel // returns a TextChannel, which has .send() method
+<TextChannel>client.channels.cache.get('12345678912') // returns a TextChannel, which has .send() method
+*/
 
-// while(true) {} also returns never
+const myInputHTMLElement = document.getElementById("myInp"); // Returns HTMLElement
+const myInput1 = <HTMLInputElement>document.getElementById("myInp"); // Returns HTMLInputElement
+const myInput2 = document.getElementById("myInp") as HTMLInputElement; // Returns HTMLInputElement
