2e8f8b402de1ccc054d849dbafb71b99c424cae4
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-10-31 15:57:09 +1100

    Tuple and Array Type Checking

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
-const person = {
+const person: {
+  name: string;
+  favNumber: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Tyson",
   favNumber: 4,
   hobbies: ["Coding", "Gaming"], // Type string[]
+  role: [2, "author"], // type (number | string)[]
 };
+
+person.role.push("admin"); // Only want 2 elements
 
 let favoriteActivities: any[]; // Anything can go in array
 favoriteActivities = ["Hey", 25];
-
-for (let hobbie of person.hobbies) {
-  console.log(hobbie.toLowerCase()); // Already knows person.hobbies is type string[]
-}
 
 console.log(person.name);
